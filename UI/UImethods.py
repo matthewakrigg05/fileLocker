08ac20c5238415d144db8ef3e12fe6e94c435a76
Matthew Akrigg <138000221+matthewakrigg05@users.noreply.github.com>
2024-08-22 15:14:19 +0100

    feat: only keeping necessary fields and titles

--- UI/UImethods.py
+++ UI/UImethods.py
@@ -11,18 +11,14 @@ def close_win(top):
     top.destroy()
 
 
-def insert_val(e):
-    e.insert(0, "Hello World!")
-
-
 def popUpBox(root):
 
     top = Toplevel(root)
-    top.geometry("750x250")
+    top.geometry("250x125")
+    top.title("File locker: Add to list")
 
     entry = Entry(top, width=25)
     entry.pack(pady=25, side=TOP)
 
-    Button(top, text="Insert", command=lambda: insert_val(entry)).pack(pady=5, side=TOP)
-    button = Button(top, text="Ok", command=lambda: close_win(top))
+    button = Button(top, text="Add to List!", command=lambda: close_win(top))
     button.pack(pady=5, side=TOP)
