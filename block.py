764f2bdbfd1546edbbdb57e336c38dfd61e0ebea
Matthew Akrigg <138000221+matthewakrigg05@users.noreply.github.com>
2024-09-20 09:38:27 +0100

    fix: integrated to view items and removed unused code

--- block.py
+++ block.py
@@ -1,7 +1,5 @@
 import subprocess
 
-import checkFiles
-
 
 def validTimeToBlock(t):
     try:
@@ -15,13 +13,13 @@ def validTimeToBlock(t):
             return True
 
 
-def closeAppIfDetected(appsToClose=checkFiles.lockedAppsContent()):
+def closeAppIfDetected(appsToClose):
     for apps in appsToClose:
         if apps in str(subprocess.check_output('tasklist')):
             subprocess.call("TASKKILL /F /IM " + apps, shell=True)
 
 
-def blockWebsites(websitesToBlock=checkFiles.lockedDomainsContent()):
+def blockWebsites(websitesToBlock):
     for website in websitesToBlock:
         with open("C:\\Windows\\System32\\drivers\\etc\\hosts", 'w') as file:
             file.write("127.0.0.1" + " " + website + " www." + website)
