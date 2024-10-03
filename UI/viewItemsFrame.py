dc48993b02243cf4ff260b1eca7b0372ba9ad554
Matthew Akrigg <matthewakrigg05@gmail.com>
2024-10-03 18:40:49 +0100

    fix: old labels disappear when changed

--- UI/viewItemsFrame.py
+++ UI/viewItemsFrame.py
@@ -16,25 +16,28 @@ class viewItemsFrame(Toplevel):
         self.resizable(False, False)
         self.protocol("WM_DELETE_WINDOW", lambda arg=self: self.onClose())
 
+        self.label = Label(self, text="Please select a list that you wish to look at.")
+        self.label.pack(side=TOP, anchor=N)
+
         if len(os.listdir("./savedLists")) == 0:
-            label = Label(self, text="You currently have no lists.")
-            label.pack(pady=5, side=TOP, anchor=NW)
+            self.label = Label(self, text="You currently have no lists.")
+            self.label.pack(pady=5, side=TOP, anchor=NW)
         else:
             self.item = StringVar()
             self.combo = ttk.Combobox(self, state="readonly", values=os.listdir("./savedLists"), textvariable=self.item)
-            self.combo.bind("<<ComboboxSelected>>", self.onChange)
+            self.combo.bind("<<ComboboxSelected>>", partial(self.onChange, self.label))
+            self.combo.pack(side=TOP, anchor=N, pady=5)
 
-            self.combo.pack(side=TOP, anchor=N)
-
-    def onChange(self, event):
+    def onChange(self, event, label):
+        self.label.pack_forget()
         selection = self.combo.get()
-        l = blockingList(selection)
+        contentList = blockingList(selection)
 
-        label = Label(self,
-                      text=("In " + selection + " there is: " + l.getListContents()),
-                      wraplength=250,
-                      justify=LEFT)
-        label.pack(side=TOP, anchor=W)
+        self.label = Label(self,
+                           text=("In " + selection + " there is: " + contentList.getListContents()),
+                           wraplength=250,
+                           justify=LEFT)
+        self.label.pack(side=BOTTOM, anchor=W)
 
     def onClose(self):
         self.destroy()
