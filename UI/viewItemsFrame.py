096ec07bf6c76e5e0a277ea022a12ac3db84a9ad
Matthew Akrigg <138000221+matthewakrigg05@users.noreply.github.com>
2024-10-03 21:53:21 +0100

    feat: getting app names

--- UI/viewItemsFrame.py
+++ UI/viewItemsFrame.py
@@ -10,22 +10,20 @@ class viewItemsFrame(Toplevel):
         super().__init__()
         self.original_frame = mainFrame
         self.geometry("250x125")
-        self.title("FileLocker: View Items")
+        self.title("View Items")
         self.resizable(False, False)
         self.protocol("WM_DELETE_WINDOW", lambda arg=self: self.onClose())
-
         self.titleLabel = Label(self, text="Please select a list that you wish to look at.")
         self.titleLabel.pack(side=TOP, anchor=N)
 
         if len(os.listdir("./savedLists")) == 0:
             self.label = Label(self, text="You currently have no lists.")
-            self.label.pack(pady=5, side=TOP, anchor=NW)
+            self.label.pack(pady=5, side=TOP, anchor=N)
         else:
             self.item = StringVar()
             self.combo = ttk.Combobox(self, state="readonly", values=os.listdir("./savedLists"), textvariable=self.item)
             self.combo.bind("<<ComboboxSelected>>", self.onChange)
             self.combo.pack(side=TOP, anchor=N, pady=5)
-
             self.label = Label(self)
 
     def onChange(self, event):
@@ -49,4 +47,3 @@ class viewItemsFrame(Toplevel):
     def onClose(self):
         self.destroy()
         self.original_frame.show()
-
